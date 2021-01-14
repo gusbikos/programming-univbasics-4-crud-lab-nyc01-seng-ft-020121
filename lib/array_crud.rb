cc2a98db87219d5b9fd46899da43cd9d0121bd90
Gus Bikos <gusbikos@gmail.com>
2021-01-14 05:00:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,8 @@ array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-
+list = ["hi", "how", "are", "you", "doing"]
+list.pop
 end
 
 def remove_element_from_start_of_array(array)
