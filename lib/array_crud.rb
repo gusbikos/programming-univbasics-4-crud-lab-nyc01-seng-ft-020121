0fe714bca9f556fdab717baa55c4b43108060f41
Gus Bikos <gusbikos@gmail.com>
2021-01-14 04:57:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
 list = ["hi", "add", "me", "to", "front"]
-list.unshift("hello")
+list.shift("hello")
 end
 
 def remove_element_from_end_of_array(array)
