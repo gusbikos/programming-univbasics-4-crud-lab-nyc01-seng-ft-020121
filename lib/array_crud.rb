1abedc509762814812587c343c8a7911a6e84006
Gus Bikos <gusbikos@gmail.com>
2021-01-14 04:59:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
 array = ["hi", "add", "me", "to", "front"]
-array.unshift('hello')
+array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
