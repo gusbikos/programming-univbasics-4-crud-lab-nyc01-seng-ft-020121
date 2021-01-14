36fdb3e7dc0b1d290ca65c4664dc4560c83ce616
Gus Bikos <gusbikos@gmail.com>
2021-01-14 04:50:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,12 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-list = [1,2,3,4]
+list = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-list = [1,2,3,4]
-list.push("arrays!")
+list = [1, 2, 3, 4]
+list.push(5)
 
 end
 
